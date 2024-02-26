de20d35cda700605e3e54a02c52b868225a4b0ce
Bisma786 <159830145+Bisma786@users.noreply.github.com>
2024-02-26 21:19:17 +0500

    Initial commit

---
+++ ex7.js
@@ -0,0 +1,9 @@
+// Q:7 Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+var additionResult = 4 + 4;
+console.log("Addition Result:", additionResult);
+var subtractionResult = 10 - 2;
+console.log("Subtraction result:", subtractionResult);
+var multiplicationResult = 2 * 4;
+console.log("Multiplication result:", multiplicationResult);
+var divisionResult = 16 / 2;
+console.log("Division result:", divisionResult);
---
+++ ex7.ts
@@ -0,0 +1,13 @@
+// Q:7 Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+let additionResult= 4+4
+console.log("Addition Result:"additionResult)
+
+let subtractionResult=10-2
+console.log("Subtraction result:"subtractionResult)
+
+let multiplicationResult=2*4
+console.log("Multiplication result:"multiplicationResult)
+
+let divisionResult=16/2
+console.log("Division result:"divisionResult)
